d43d4420c5ae7ce0eb1a0eeb94345c427c9a728d
LRFSousa <luisricardofs@gmail.com>
2023-10-04 21:02:16 -0300

    Classe p/ conexão com BD

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -1,5 +1,12 @@
 package db;
 
-public class DbException {
+public class DbException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DbException(String msg) {
+		super(msg);
+	}
+	
 
 }
